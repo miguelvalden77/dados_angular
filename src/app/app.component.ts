71dedf04a84ef71025b7b156dc8dbd90d0c9e2e8
miguelvalden <miguelvalden77@gmail.com>
2023-09-05 20:23:39 +0200

    dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dados';
+  number1 = 1
+  number2 = 3
+  win = false
+
+  tirarDados() {
+    this.win = false
+
+    this.number1 = Math.ceil(Math.random() * 6)
+    this.number2 = Math.ceil(Math.random() * 6)
+
+    if (this.number1 === this.number2) this.win = true
+  }
 }
